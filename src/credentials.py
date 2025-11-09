8cb7938ae58a0c57d5bcc835d5b56e1d4c9d7344
GGleee1 <127374022+GGleee1@users.noreply.github.com>
2025-11-09 10:02:56 +0800

    Create credentials.py

---
+++ src/credentials.py
@@ -0,0 +1,11 @@
+#Live
+#APIKEY = ""
+#APISECRET = ""
+#ACCOUNTID = 123456 #integer
+#WEBHOOK = ""
+
+#Testnet 
+APIKEY = ""
+APISECRET = ""
+ACCOUNTID = 123456
+WEBHOOK = "" #url to post messages to
